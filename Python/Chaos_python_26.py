33e8f08f0b32fd062f90925242c9615ff9018b70
Ilya Rubinshteyn <ilya_rubinshteyn@ilya-2.aeriagames.local>
2015-04-15 11:44:18 -0700

    modified the chaos for both pythons

--- Python/Chaos_python_26.py
+++ Python/Chaos_python_26.py
@@ -1,4 +1,6 @@
-def main():
+#For Python 2.x
+
+def Chaos26():
 	print "This program illustrates a chaotic function"
 	x = input("Enter a number between 0 and 1: ")
 	y = input("Enter a second number between 0 and 1: ")
@@ -6,4 +8,4 @@ def main():
 		x = 3.9 * x * (1 - x)
 		y = 3.3 * y * (1 - y)
 		print x, '\t',  y
-main()
+Chaos26()
--- Python/Chaos_python_34.py
+++ Python/Chaos_python_34.py
@@ -1,4 +1,6 @@
-def main():
+#For Python 3.x
+
+def Chaos34():
         print ("This program illustrates a chaotic function")
         x = eval(input("Enter a number between 0 and 1: "))
         y = eval(input("Enter a second number between 0 and 1: "))
@@ -7,4 +9,4 @@ def main():
                 y = 3.3 * (y - y * y)
                 print (x,'\t',y)
 
-main()
+Chaos34()
